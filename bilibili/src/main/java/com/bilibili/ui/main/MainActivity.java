79a3bc383d373af7fa9872ed72e1132cd99c8d4c
miserydx <miserydx@163.com>
2017-06-29 15:05:13 +0800

    feature: 去掉首页侧滑抽屉栏的滚动条，布局文件名字更正

--- bilibili/src/main/java/com/bilibili/ui/main/MainActivity.java
+++ bilibili/src/main/java/com/bilibili/ui/main/MainActivity.java
@@ -3,11 +3,11 @@ package com.bilibili.ui.main;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.internal.NavigationMenuView;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.FrameLayout;
 
 import com.bilibili.App;
@@ -52,52 +52,15 @@ public class MainActivity extends SupportActivity implements IBaseMvpActivity<Ma
         mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                // Handle navigation view item clicks here.
-                int id = item.getItemId();
-
-                if (id == R.id.nav_camera) {
-                    // Handle the camera action
-                } else if (id == R.id.nav_gallery) {
-
-                } else if (id == R.id.nav_slideshow) {
-
-                } else if (id == R.id.nav_manage) {
-
-                } else if (id == R.id.nav_share) {
-
-                } else if (id == R.id.nav_send) {
-
-                }
                 mDrawerLayout.closeDrawer(GravityCompat.START);
                 return true;
             }
         });
-        mDrawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
-            @Override
-            public void onDrawerSlide(View drawerView, float slideOffset) {
-                //content布局随抽屉布局移动
-                //获取屏幕的宽高
-//                WindowManager manager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
-//                Display display = manager.getDefaultDisplay();
-                //设置右面的布局位置  根据左面菜单的right作为右面布局的left   左面的right+屏幕的宽度（或者right的宽度这里是相等的）为右面布局的right
-//                mLinearLayout.layout(mNavigationView.getRight(), 0, mNavigationView.getRight() +  display.getWidth(), display.getHeight());
-            }
-
-            @Override
-            public void onDrawerOpened(View drawerView) {
-
-            }
-
-            @Override
-            public void onDrawerClosed(View drawerView) {
-
-            }
-
-            @Override
-            public void onDrawerStateChanged(int newState) {
-
-            }
-        });
+        //隐藏NavigationView右侧滚动条
+        NavigationMenuView navigationMenuView = (NavigationMenuView) mNavigationView.getChildAt(0);
+        if (navigationMenuView != null) {
+            navigationMenuView.setVerticalScrollBarEnabled(false);
+        }
     }
 
     @Override
